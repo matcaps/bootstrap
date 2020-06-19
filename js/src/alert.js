b96507cb2b620d19245fa418e9dfd7701eb00b65
XhmikosR <xhmikosr@gmail.com>
2020-06-19 18:21:24 +0300

    Revert "Switch to `Node#append()` / `Node#remove()`"

--- js/src/alert.js
+++ js/src/alert.js
@@ -103,7 +103,7 @@ class Alert {
 
   _destroyElement(element) {
     if (element.parentNode) {
-      element.remove()
+      element.parentNode.removeChild(element)
     }
 
     EventHandler.trigger(element, EVENT_CLOSED)
